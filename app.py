5452d4fa35017929c814c36ad02c5cfc5f948ebd
Priyanshu Sharma <ps4798214@gmail.com>
2023-12-05 23:47:22 +0530

    add create_item(name)

--- app.py
+++ app.py
@@ -25,3 +25,12 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
