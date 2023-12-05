8616ec68bf44c4364855c6ee938be8eec6c80aa9
Priyanshu Sharma <ps4798214@gmail.com>
2023-12-06 00:07:24 +0530

    add get_store(name)

--- app.py
+++ app.py
@@ -34,3 +34,10 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item, 201
     return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
