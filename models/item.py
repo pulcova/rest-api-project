0756fbbc9c4f36f2557a6cd334ede2bf552ab738
Priyanshu Sharma <ps4798214@gmail.com>
2023-12-10 18:24:07 +0000

    add configuration SQLAlchemy

--- models/item.py
+++ models/item.py
@@ -6,6 +6,6 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-    store_id = db.column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
 
-    store = db.realtionship("StoreModel", back_populates="items")
+    store = db.relationship("StoreModel", back_populates="items")
